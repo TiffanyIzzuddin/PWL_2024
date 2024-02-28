bf0c9f7d3dda1e87bb0fc08ca57c63512df81ae5
Dodit Suprianto <doditsuprianto@gmail.com>
2024-02-28 10:15:30 +0700

    second commit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello()
+    {
+        return 'Hello World';
+    }
+
+    // public function greeting()
+    // {
+    //     return view('blog.hello', ['name' => 'Andis']);
+    // }
+
+    public function greeting()
+    {
+        return view('blog.hello')
+            ->with('name', 'Andi')
+            ->with('occupation', 'Astronaut');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -43,5 +46,80 @@ Route::delete('mahasiswa/{id}', function ($id) {
 
 Route::match(['get', 'post'], '/specialUrl', function () {
 });
+
 Route::any('/specialMahasiswa', function ($id) {
 });
+
+// soal latihan
+Route::get('/user/{name}', function ($name) {
+    return 'Tiffany ' . $name;
+});
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+// });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+// soal praktikum
+Route::get('/user/{name?}', function ($name = 'Tiffany') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId = '1', $commentId = '5') {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+// http://127.0.0.1:8000/posts/1/comments/5
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+// Generating URLs...
+// $url = route('profile');
+// Generating Redirects...
+// return redirect()->route('profile');
+
+// route group
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
+// Redirect Routes
+Route::redirect('/here', '/there');
+// View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Andi']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
